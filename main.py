2a9bc1d3589e9bf2f567e420fb10d2722ee3e15d
Leonardo Mazzone <leo@mazzone.space>
2025-04-11 13:37:43 +0100

    Abstract DB description

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from sqlalchemy import create_engine, inspect
 import json
 
-def main():
-    engine = create_engine('sqlite:///chinook.db')
+from sqlalchemy import Engine, create_engine, inspect
+
+
+def describe_db(engine: Engine) -> str:
     inspector = inspect(engine)
     tables_info = {}
     table_names = inspector.get_table_names()
-    
+
     for table_name in table_names:
         columns = inspector.get_columns(table_name)
-        
+
         column_info = [
             {
                 "name": column["name"],
@@ -17,10 +18,15 @@ def main():
             }
             for column in columns
         ]
-        
+
         tables_info[table_name] = column_info
-    
-    print(json.dumps(tables_info, indent=4))
+
+    return json.dumps(tables_info, indent=4)
+
+
+def main():
+    engine = create_engine("sqlite:///chinook.db")
+    print(describe_db(engine))
 
 
 if __name__ == "__main__":
