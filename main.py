e12eb8d4f128e98ec47e7af2335d3358a8c737b8
Leonardo Mazzone <leo@mazzone.space>
2025-04-11 14:58:27 +0100

    Implement 3 nodes

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 
+from squirrel import prompts
 from squirrel.db import describe_db, results_as_str
 
 ENGINE = engine = create_engine("sqlite:///chinook.db")
+ENGINE_DESC = describe_db(ENGINE)
 
 
 class GraphState(BaseModel):
     question: str | None = None
     db_description: str | None = None
+    valid: bool | None = None
     results: str | None = []
 
 
+def validator_node(state: GraphState) -> dict[str, str]:
+    state.valid = prompts.question_validation_chain().invoke(
+        {
+            "context": ENGINE_DESC,
+            "question": state.question,
+        }
+    )
+    return {"valid": state.valid}
+
+
 def retriever_node(state: GraphState) -> dict[str, str]:
-    state.results = results_as_str(sql=state.sql, engine=ENGINE)
+    sql = prompts.sql_chain().invoke(
+        {
+            "context": ENGINE_DESC,
+            "question": state.question,
+        }
+    )
+    state.results = results_as_str(sql=sql, engine=ENGINE)
     return {"results": state.results}
+
+
+def generation_node(state: GraphState) -> dict[str, str]:
+    generation = prompts.rag_chain().invoke(
+        {
+            "context": state.results,
+            "question": state.question,
+        }
+    )
+    return {"generation": generation}
 
 
 def main():
